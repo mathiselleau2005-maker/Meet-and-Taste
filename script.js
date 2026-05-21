d70e3dff6a47488c96df44dde146c135abe60e7f
mathiselleau2005-maker <mathis.elleau2005@gmail.com>
2026-05-22 00:38:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const reservationButton = document.querySelector(".submit-btn");
+const reservationButton = document.querySelector(".reservation-form button");
 
 reservationButton.addEventListener("click", () => {
-
   alert("Merci 🌼 Votre demande de réservation a bien été prise en compte.");
+});
 
+const daisies = document.querySelectorAll(".daisy-item");
+
+daisies.forEach((daisy) => {
+  daisy.addEventListener("click", () => {
+    const targetId = daisy.dataset.target;
+    const section = document.getElementById(targetId);
+
+    if(section){
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
+  });
 });
